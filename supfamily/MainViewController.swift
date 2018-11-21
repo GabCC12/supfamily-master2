fe1b44b53a9247956d2718b1cb8a50cd778159f0
GabCC12 <293290@supinfo.com>
2018-11-21 15:13:10 +0100

    Initial commit

---
+++ supfamily/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  supfamily
+//
+//  Created by Supinfo on 13/11/2018.
+//  Copyright © 2018 Supinfo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var buttonConnect: UIButton!
+    
+    
+    @IBAction func connection(_ sender: Any) {
+        /*guard username.text != nil , password.text != nil else {
+            print("Error login or password incorrect")
+            return
+        }*/
+    }
+}
